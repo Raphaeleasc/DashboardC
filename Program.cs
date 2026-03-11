fcc6da3695b075c05120acc6eb115a22e951c73e
Raphael Breton <raphael.breton@ikmail.com>
2026-03-11 11:30:13 +0100

    INIT : project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+class Switch{
+    private bool hasNic = true;
+    private bool hasMacAdresse = true;
+    public bool GetMacAdresse(){
+        return hasMacAdresse;
+    }
+    public bool GetNic(){
+        return hasNic;
+    }
+}
+
+class Program{
+    public static void Main(){
+    Console.WriteLine("Hello ");
+   }
+}
